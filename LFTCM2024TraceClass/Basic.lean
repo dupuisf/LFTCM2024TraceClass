7d6b43e6816f613b3e8515ff2193a3c9b64f5244
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2024-03-25 11:48:51 +0100

    add stub

--- LFTCM2024TraceClass/Basic.lean
+++ LFTCM2024TraceClass/Basic.lean
@@ -1,1 +1,12 @@
-def hello := "world"
+import Mathlib.Topology.ContinuousFunction.FunctionalCalculus
+import Mathlib.Analysis.NormedSpace.Star.ContinuousFunctionalCalculus.Instances
+
+namespace ContinuousLinearMap
+
+variable {𝕜 E : Type*} [IsROrC 𝕜] [NormedAddCommGroup E]
+  [InnerProductSpace 𝕜 E]
+
+/-- The trace of a continuous linear map -/
+def trace (f : E →L[𝕜] E) := sorry
+
+end ContinuousLinearMap
