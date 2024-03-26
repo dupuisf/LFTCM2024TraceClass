304f5149164922cbba7cbc64f420bfe5993307ce
Frédéric Dupuis <dupuisf@iro.umontreal.ca>
2024-03-26 11:37:47 +0100

    update lakefile

--- lakefile.lean
+++ lakefile.lean
@@ -4,11 +4,12 @@ open Lake DSL
 package «LFTCM2024TraceClass» where
   -- add package configuration options here
 
+@[default_target]
 lean_lib «LFTCM2024TraceClass» where
   -- add library configuration options here
 
-@[default_target]
-lean_exe «lftcm2024traceclass» where
-  root := `Main
+--@[default_target]
+--lean_exe «lftcm2024traceclass» where
+--  root := `Main
 
 require mathlib from git "https://github.com/leanprover-community/mathlib4.git" @ "1640667583db65dab892533b0059dac5c0a67270"
